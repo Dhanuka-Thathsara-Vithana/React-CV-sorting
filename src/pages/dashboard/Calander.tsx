eeaed9b55704ffb35984fe5d7a43411e533cbc90
DTvithana <dhanukavithana@gmail.com>
2023-08-27 10:32:25 +0530

    Add-Dynamic-calander

--- src/pages/dashboard/Calander.tsx
+++ src/pages/dashboard/Calander.tsx
@@ -1,11 +1,94 @@
+import * as React from 'react';
+import { Dayjs } from 'dayjs';
+import Badge from '@mui/material/Badge';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { PickersDay, PickersDayProps } from '@mui/x-date-pickers/PickersDay';
+import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
+import { DayCalendarSkeleton } from '@mui/x-date-pickers/DayCalendarSkeleton';
+import { Card } from '@mui/material';
 
 
-function Calander() {
-  return (
-     <h1>
-        Hi
-     </h1>
-  )
+function fakeFetch(date: Dayjs, { signal }: { signal: AbortSignal }) {
+  return new Promise<{ daysToHighlight: number[] }>((resolve, reject) => {
+    const timeout = setTimeout(() => {
+      const daysToHighlight = [1, 2, 8];
+
+      resolve({ daysToHighlight });
+    }, 500);
+
+    signal.onabort = () => {
+      clearTimeout(timeout);
+      reject(new DOMException('aborted', 'AbortError'));
+    };
+  });
 }
 
-export default Calander
+function ServerDay(props: PickersDayProps<Dayjs> & { highlightedDays?: number[] }) {
+  const { highlightedDays = [], day, outsideCurrentMonth, ...other } = props;
+
+  const isSelected =
+    !props.outsideCurrentMonth && highlightedDays.indexOf(props.day.date()) >= 0;
+
+  return (
+    <Badge
+      key={props.day.toString()}
+      overlap="circular"
+      badgeContent={isSelected ? '🌚' : undefined}
+    >
+      <PickersDay {...other} outsideCurrentMonth={outsideCurrentMonth} day={day} />
+    </Badge>
+  );
+}
+
+export default function Calendar() {
+  const requestAbortController = React.useRef<AbortController | null>(null);
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [highlightedDays, setHighlightedDays] = React.useState([1, 2, 15]);
+
+  const fetchHighlightedDays = (date: Dayjs) => {
+    const controller = new AbortController();
+    fakeFetch(date, {
+      signal: controller.signal,
+    })
+      .then(({ daysToHighlight }) => {
+        setHighlightedDays(daysToHighlight);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        // ignore the error if it's caused by `controller.abort`
+        if (error.name !== 'AbortError') {
+          throw error;
+        }
+      });
+
+    requestAbortController.current = controller;
+  };
+
+
+  const handleMonthChange = (date: Dayjs) => {
+
+    setIsLoading(true);
+    fetchHighlightedDays(date);
+  };
+
+  return (
+    <Card>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <DateCalendar
+        loading={isLoading}
+        onMonthChange={handleMonthChange}
+        renderLoading={() => <DayCalendarSkeleton />}
+        slots={{
+          day: ServerDay,
+        }}
+        slotProps={{
+          day: {
+            highlightedDays,
+          } as any,
+        }}
+      />
+    </LocalizationProvider>
+    </Card>
+  );
+}
--- src/pages/dashboard/dashboard.tsx
+++ src/pages/dashboard/dashboard.tsx
@@ -8,18 +8,20 @@ function Dashboard() {
     <>
     <Container maxWidth="lg">
       <Grid
+        width='1800px'
         paddingTop='20px'
         container
         direction="row"
         justifyContent="center"
         alignItems="stretch"
-        spacing={3}
+        spacing={5}
+        paddingLeft='23rem'
       >
         <Grid item lg={8} xs={12}>
         <UserPerformance/>
         </Grid>
         <Grid item lg={3} xs={12}>
-          {/* <Calander/> */}
+          <Calander/>
         </Grid>
         <Grid item lg={25} xs={12}>
         {/* <JobPostGrid/> */}
