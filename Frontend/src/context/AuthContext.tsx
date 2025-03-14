6572e25aea4d696d0b73843bc84f209083103b37
DT VITHANA <dhanukavithana@gmail.com>
2025-03-14 23:51:20 +0530

    remove base url due to issue

--- Frontend/src/context/AuthContext.tsx
+++ Frontend/src/context/AuthContext.tsx
@@ -9,7 +9,7 @@ interface AuthContextType {
   login: (data: any) => Promise<void>;
   logout: () => void;
 }
-const baseUrl = import.meta.env.VITE_API_BASE_URL;
+
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 
@@ -37,7 +37,7 @@ const AuthProviderWithRouter = ({ children }: { children: React.ReactNode }) => 
         
         axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
         
-        const response = await axios.get(`${baseUrl}/api/auth/user`, { withCredentials: true });
+        const response = await axios.get('http://localhost:5000/api/auth/user', { withCredentials: true });
         setUser(response.data);
         setAuthToken(token);
       } catch (error) {
@@ -54,7 +54,7 @@ const AuthProviderWithRouter = ({ children }: { children: React.ReactNode }) => 
 
   const login = async (data: any) => {
     try {
-      const response = await axios.post(`${baseUrl}/api/auth/`, data, { withCredentials: true });
+      const response = await axios.post('http://localhost:5000/api/auth/', data, { withCredentials: true });
       const { userObj: userData, token } = response.data;
      
       // Store token
@@ -76,7 +76,7 @@ const AuthProviderWithRouter = ({ children }: { children: React.ReactNode }) => 
 
   const logout = async () => {
     try {
-      await axios.post(`${baseUrl}/api/auth/logout`, {}, { withCredentials: true });
+      await axios.post('http://localhost:5000/api/auth/logout', {}, { withCredentials: true });
     } catch (error) {
       console.error('Logout API call failed:', error);
     } finally {
--- Frontend/src/pages/education/Education.tsx
+++ Frontend/src/pages/education/Education.tsx
@@ -7,8 +7,6 @@ import PropTypes from 'prop-types';
 import axios, { CanceledError } from 'axios';
 import { useAuth } from '../../context/AuthContext';
 
-const baseUrl = import.meta.env.VITE_API_BASE_URL;
-
 interface EduProps{
   _id: string,
   Institution: string,
@@ -63,7 +61,7 @@ function Education() {
       const originalWork = [...education];
       setEducation(education.filter(u => u._id !== id));
 
-      axios.delete(`${baseUrl}/${id}` )
+      axios.delete(`http://localhost:5000/api/education/${id}` )
       .catch(err => {
         setError(err.message);
         setEducation(originalWork)
